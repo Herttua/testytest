11bfd417fc55e65a15c786e8bed86d9d78134eaf
Herttua <juha.salinmaki@gmail.com>
2018-08-09 12:15:03 +0300

    Making loadbar

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while(!operation.isDone)
+        {
+
+        }
+
+        yield return null;
+    }
+}
